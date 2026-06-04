a30a1722f093976adca011d9c7b469fcaf4ed98d
Rooster123872 <rsrenatoalves@gmail.com>
2026-06-03 20:26:10 -0400

    Primeira versao do portscanning com timeout

---
+++ portscanning.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import socket, sys
+
+print ("==== ROOSTER SEC =====")
+print ("Portscaning em Python")
+print ("======  1.0 VERSION ==========")
+
+ip = input("Digite o IP que você quer verificar as portas abertas ")
+portas_totais = range(1, 6500)
+
+for porta in portas_totais:
+    meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    meusocket.settimeout(0.1)
+    res = meusocket.connect_ex((ip,porta))
+    if res == 0:
+        print (" Porta ", porta, "ABERTA! [*] ")
+    meusocket.close()
+    
+    ## OBRIGADO POR TER LIDO MEU SCRIPT NO GIT HUB (=
